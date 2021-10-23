8f05f1c7a396ebdd82edb373baba2badf73afa77
Mahsa <mahsa.alikhany@gmail.com>
2021-10-23 20:47:21 +0330

    created Employee

---
+++ src/ir/maktab/model/Employee.java
@@ -0,0 +1,46 @@
+package ir.maktab.model;
+
+import java.sql.Date;
+
+/**
+ * @author Mahsa Alikhani m-58
+ */
+public class Employee extends Person{
+
+    private String personalCode;
+    private int salary;
+    private Date hiredDate;
+    private int personId;
+
+    public String getPersonalCode() {
+        return personalCode;
+    }
+
+    public void setPersonalCode(String personalCode) {
+        this.personalCode = personalCode;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Date getHiredDate() {
+        return hiredDate;
+    }
+
+    public void setHiredDate(Date hiredDate) {
+        this.hiredDate = hiredDate;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+}
