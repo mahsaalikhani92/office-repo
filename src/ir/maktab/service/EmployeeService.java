27a5d641f4c9f535eaefeda3150aafd27c425d4c
Mahsa <mahsa.alikhany@gmail.com>
2021-10-23 22:57:41 +0330

    added getAllFiveLastYears to EmployeeService

--- src/ir/maktab/service/EmployeeService.java
+++ src/ir/maktab/service/EmployeeService.java
@@ -1,7 +1,22 @@
 package ir.maktab.service;
 
+import ir.maktab.model.Employee;
+import ir.maktab.repository.EmployeeDao;
+
+import java.util.List;
+
 /**
  * @author Mahsa Alikhani m-58
  */
 public class EmployeeService {
+    private final EmployeeDao employeeDao;
+
+    public EmployeeService() {
+        employeeDao = new EmployeeDao();
+    }
+
+    public List<Employee> getAllFiveLastYears() {
+        List<Employee> employeeList = employeeDao.findAllFiveLastYears();
+        return employeeList;
+    }
 }
