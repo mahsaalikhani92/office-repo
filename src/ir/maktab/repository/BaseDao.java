88738d538159f475e11cd8db948f1a44955e2249
Mahsa <mahsa.alikhany@gmail.com>
2021-10-23 23:08:01 +0330

    created BaseDao

---
+++ src/ir/maktab/repository/BaseDao.java
@@ -0,0 +1,24 @@
+package ir.maktab.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author Mahsa Alikhani m-58
+ */
+public class BaseDao {
+    private Connection connection;
+
+    public BaseDao() throws SQLException, ClassNotFoundException {
+        connection = getConnection();
+    }
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        if(connection == null){
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital", "root", "5103583");
+        }
+        return connection;
+    }
+}
